49012e61d334a3a97a93892dfe4ddd7fb863ac65
iryna-arkhanhelska <43938180+iryna-arkhanhelska@users.noreply.github.com>
2018-11-19 01:28:01 +0200

    Update main.js

--- my_project/js/main.js
+++ my_project/js/main.js
@@ -8,3 +8,16 @@ $('body,html').animate({
 scrollTop:$('#' + $(this).data('value')).offset().top
 },1000);
 })
+
+$(function(){
+
+    $('.menuToggle').on('click', function() {
+       $('.menu').slideToggle(300, function(){
+            if( $(this).css('display') === "none"){
+                $(this).removeAttr('style');
+            }
+       });
+
+    });
+
+});
